d2ea64acaa46d7161421e61bb25e7824ab57e3ab
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-10 16:27:59 -0300

    feat: logica para renderizar as imagens do pokemon

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,10 +12,34 @@
             </form>
         </div>
         <div class="list-container">
-            <ul v-for='item in pokemon' v-bind:key='item.id' >
+            <ul v-for='item in pokemon' v-bind:key='item.id'>
                 <li> 
                     <img :src='item.sprites.front_default' :alt='item.name'/>
-                    <span>{{ item.name}}</span>
+                    <span>{{item.name}}</span>
+                </li>
+                <li v-if="generation1['red-blue'].front_default">
+                   <img :src="generation1['red-blue'].front_default" :alt="`${item.name} - Geração 1`"> 
+                   <span>{{item.name}} - Geração 1</span>
+                </li>
+                <li v-if="generation2['silver'].front_default">
+                   <img :src="generation2['silver'].front_default" :alt="`${item.name} - Geração 2`"> 
+                   <span>{{item.name}} - Geração 2</span>
+                </li>
+                <li v-if="generation3['ruby-sapphire'].front_default">
+                   <img :src="generation3['ruby-sapphire'].front_default" :alt="`${item.name} Geração 3`"> 
+                   <span>{{item.name}} - Geração 3</span>
+                </li>
+                <li v-if="generation4['heartgold-soulsilver'].front_default">
+                   <img :src="generation4['heartgold-soulsilver'].front_default" :alt="`${item.name} Geração 4`"> 
+                   <span>{{item.name}} - Geração 4</span>
+                </li>
+                <li v-if="generation5['black-white'].animated.front_default">
+                   <img :src="generation5['black-white'].animated.front_default" :alt="`${item.name} Geração 5`"> 
+                   <span>{{item.name}} - Geração 5</span>
+                </li>
+                <li v-if="generation6['omegaruby-alphasapphire'].front_default">
+                   <img :src="generation6['omegaruby-alphasapphire'].front_default" :alt="`${item.name} Geração 6`"> 
+                   <span>{{item.name}} - Geração 6</span>
                 </li>
             </ul>
         </div>
@@ -31,7 +55,13 @@ export default {
     data () {
         return {
             name_pokemon: null,
-            pokemon: []
+            pokemon: [],
+            generation1: [],
+            generation2: [],
+            generation3: [],
+            generation4: [],
+            generation5: [],
+            generation6: [],
         }
     },
     methods: {
@@ -47,6 +77,13 @@ export default {
                 name, 
                 sprites
             }]
+            this.generation1 = sprites.versions['generation-i']
+            this.generation2 = sprites.versions['generation-ii']
+            this.generation3 = sprites.versions['generation-iii']
+            this.generation4 = sprites.versions['generation-iv']
+            this.generation5 = sprites.versions['generation-v']
+            this.generation6 = sprites.versions['generation-vi']
+            
         },
     },
 }
