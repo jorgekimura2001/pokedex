b602bfdaf8fc0e4582f2a9295099491c0f066182
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-10 17:03:34 -0300

    style: estilizaco do header

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,22 @@ export default {
   components: {
     Header,
     Main
-}
+  },
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  border: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,32 @@
         <header>
             <h1>PokeDex</h1>
         </header>
+        <img src='../assets/pokeball.svg' alt="">
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Header-Component'
+    name: 'Header-Component',
+    props: {
+        logo: String
+    }
 }
 </script>
 
 <style scoped>
-
+    .header{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background: #E83838;
+        color: white;
+        padding: 20px;
+        margin-bottom: 20px;
+    }
+    .header img{
+        width: 3.125rem;
+        height: 3.125rem;
+    }
 </style>
